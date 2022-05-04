3d9213bf2c031b326d7b610ec177f0a758c5d29e
Spidey <87893142+Perry-xD@users.noreply.github.com>
2022-05-04 17:28:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ from requests.structures import CaseInsensitiveDict
 
 
 
-API_ID = os.environ.get('API_ID')
-API_HASH = os.environ.get('API_HASH')
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+API_ID = int(os.environ.get('API_ID', 0))
+API_HASH = os.environ.get('API_HASH', None)
+BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 
 god = c("godboyx", API_ID, API_HASH, BOT_TOKEN)
 
